a5bfb2f2f2c8a5f3c8ab6bfec2b82595e79405a7
Roman Yakushev <boxboxbox@inbox.ru>
2022-10-24 20:40:53 +0300

    Добавьте файлы проекта.

---
+++ PasswordManager/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace PasswordManager.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
