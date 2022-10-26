ecac1d8c624486f83cd622840aeb6794a363180e
Roman Yakushev <boxboxbox@inbox.ru>
2022-10-26 20:21:40 +0300

    Create Add View

--- PasswordManager/Controllers/HomeController.cs
+++ PasswordManager/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace PasswordManager.Controllers
             return View();
         }
 
+        public IActionResult Add()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
--- PasswordManager/Models/Item.cs
+++ PasswordManager/Models/Item.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Login { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
 
