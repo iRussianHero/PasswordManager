98f9cfca4a27225203f0f385ca80361c5556d258
Roman Yakushev <boxboxbox@inbox.ru>
2022-10-27 22:26:47 +0300

    Add Edit Form

--- PasswordManager/Controllers/HomeController.cs
+++ PasswordManager/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Hosting;
 using PasswordManager.Models;
 using System.Diagnostics;
 using Type = PasswordManager.Models.Type;
@@ -23,10 +24,39 @@ namespace PasswordManager.Controllers
             return View(_managerDbContext.Items.ToList());
         }
 
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await DeleteById(id);
+            return View("Index", _managerDbContext.Items.ToList());
+        }
+
         public IActionResult Add()
         {
             ViewBag.Types = new SelectList(_managerDbContext.Types, "Id", "Name");
             return View();
+        }
+
+        public IActionResult Edit(string id)
+        {
+            ViewBag.Types = new SelectList(_managerDbContext.Types, "Id", "Name");
+            Item item = FindById(id);
+            return View(item);
+        }
+
+        public async Task<IActionResult> Update(Item item, string id)
+        {
+            Item oldItem = FindById(id);
+            oldItem.Name = item.Name;
+            oldItem.Description = item.Description;
+            oldItem.Password = item.Password;
+            oldItem.Email = item.Email;
+            oldItem.TypeId = item.TypeId;
+            oldItem.Login = item.Login;
+            _managerDbContext.Update(oldItem);
+            await _managerDbContext.SaveChangesAsync();
+            TempData["Status"] = "Post was updated";
+            return RedirectToAction("Index", _managerDbContext.Items.ToList());
         }
 
         public async Task<IActionResult> AddNew(Item item)
@@ -46,6 +76,24 @@ namespace PasswordManager.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> DeleteById(string id)
+        {
+            Item item = FindById(id);
+
+            _managerDbContext.Items.Remove(item);
+            await _managerDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public Item FindById(string id)
+        {
+            Item item = new Item();
+            return item = _managerDbContext.Items
+                .Where(o => o.Id.ToString() == id)
+                .FirstOrDefault();
         }
     }
 }
